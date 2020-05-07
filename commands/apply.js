eccb6c850024e4170ca71b6e1b272f8e2f5165b3
Ben Wart <benwart@gmail.com>
2020-05-06 23:27:40 -0700

    added terraform lib with updates

--- commands/apply.js
+++ commands/apply.js
@@ -1,19 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Box } from 'ink';
 import store from '../store';
-import { define, run, complete, WORK_CREATE, WORK_DESTROY } from '../store/workSlice';
 import { CompletedWork, RunningWork } from '../components';
-
-store.dispatch(define(1, 'resouce1', WORK_CREATE));
-store.dispatch(define(2, 'resouce2', WORK_CREATE));
-store.dispatch(define(3, 'resouce1', WORK_DESTROY));
-store.dispatch(run(1, 10));
-store.dispatch(run(2, 30));
-store.dispatch(run(3, 30));
-store.dispatch(complete(2, 20));
-store.dispatch(complete(1, 60));
 
 /// Terraform Apply Wrapper with Progress
 const TerraformApply = ({ varFile, module }) => {
